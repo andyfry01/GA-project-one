d80c4588bc49477762334485c03b8ced38428ead
Andrew Fry <andyfry01@gmail.com>
2016-03-03 17:10:04 -0500

    baby steps man, take it easy

--- game.js
+++ game.js
@@ -1,21 +1,37 @@
-function char(healthPoints, attackPower, healCount){
+//character object
+
+function char(healthPoints, attackPower, healCount, healPower){
   this.healthPoints = healthPoints;
   this.attackPower = attackPower;
   this.healCount = healCount;
+  this.healPower = healPower;
 
   this.attack = function(enemy) {
     enemy.healthPoints -= this.attackPower;
   }
+  this.heal = function(char) {
+    char.healthPoints +=
+  }
 };
 
+
+//enemy object
 function enemy(healthPoints, attackPower){
   this.healthPoints = healthPoints;
   this.attackPower = attackPower;
+  this.attack = function(character) {
+    character.healthPoints -= this.attackPower;
+  }
 };
 
+
+//tests
 var andy = new char(10, 2, 3);
 console.log(andy);
 var baddie = new enemy(10, 2);
 console.log(baddie);
 
-andy.attack(enemy);
+andy.attack(baddie);
+console.log(baddie);
+baddie.attack(andy);
+console.log(andy);
--- test-area.js
+++ test-area.js
@@ -3,21 +3,22 @@ function char(healthPoints, attackPower, healCount){
   this.attackPower = attackPower;
   this.attackRange = [];
   this.healCount = healCount;
-
-
-  this.findAttackRange = function(this.attackPower){
-    var attackRange = [];
-    attackRange.push(Math.floor(Math.random() * this.attackPower));
-    if (attackRange[0] > 0) {
-      attackRange.push(attackRange[0] - (attackRange[0]/2));
-    } else {
-      attackRange.push(0)
-    }
-    console.log(attackRange);
-  }
-
-
   this.attack = function(enemy) {
     enemy.healthPoints -= this.attackPower;
   }
 };
+
+var andy = new char(10, 10, 10);
+
+
+// first attempt at setting an attack range for a character.
+// this.findAttackRange = function(attackPower){
+//   char.attackRange.push(Math.floor(Math.random() * this.attackPower));
+//   if (attackRange[0] > 0) {
+//     attackRange.push(attackRange[0] - (attackRange[0]/2));
+//   } else {
+//     attackRange.push(0)
+//   }
+//   console.log(attackRange);
+// }
+//
