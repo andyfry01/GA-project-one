28c2a00c070914c09b492012b86ab680fa2d2390
Andrew Fry <andyfry01@gmail.com>
2016-03-04 11:34:18 -0500

    morning work done, now to the setTimeout....

--- game.js
+++ game.js
@@ -1,12 +1,44 @@
 console.log("game loaded");
 
+
+/******************* basic game elements ******************/
 // Set to false if the character dies.
 var gameActive = true;
 
-// Specifies whose turn it is.
+// Specifies whose turn it is. Will alternate between player/enemy.
 var turn = "player";
 
-//character object creator
+
+
+/******************* button event listeners ******************/
+
+var attackButton = document.querySelector('#attack');
+
+attackButton.addEventListener("click", function() {
+  andy.attack(baddie);
+  console.log("attack performed, baddie health: " + baddie.healthPoints)
+
+});
+
+var defendButton = document.querySelector('#defend');
+
+defendButton.addEventListener("click", function() {
+  andy.defend();
+  console.log("defend performed.")
+});
+
+var healButton = document.querySelector('#heal');
+
+healButton.addEventListener("click", function() {
+  andy.heal(andy);
+  console.log("heal performed, andy health: " + andy.health)
+
+});
+
+
+
+
+/******************* character object creator ******************/
 function char(healthPoints, attackPower, healCount, healPower) {
 
   /*** character stats ***/
@@ -22,7 +54,7 @@ function char(healthPoints, attackPower, healCount, healPower) {
   /*** character abilities ***/
   //attack ability
   this.attack = function(enemy) {
-    if (enemy.defendStatus = true) {
+    if (enemy.defendStatus == true) {
       enemy.healthPoints -= (this.attackPower / 2); //note: possibly revisit this
     } else {
       enemy.healthPoints -= this.attackPower;
@@ -33,9 +65,11 @@ function char(healthPoints, attackPower, healCount, healPower) {
   this.heal = function(char) {
     if (this.healCount = 0) {
       console.log("no more heals left!"); //note: update to display on screen later
-    } else { char.healthPoints += this.healPower;
-    this.healCount -= 1;
-  }
+    } else {
+      char.healthPoints += this.healPower;
+      this.healCount -= 1;
+    }
+  };
 
   //defend ability
   this.defend = function(char) {
@@ -44,8 +78,9 @@ function char(healthPoints, attackPower, healCount, healPower) {
 };
 
 
-//enemy object
+/******************* enemy object creator ******************/
 function enemy(healthPoints, attackPower) {
+
   /*** enemy stats ***/
   this.healthPoints = healthPoints;
   this.attackPower = attackPower;
@@ -57,53 +92,24 @@ function enemy(healthPoints, attackPower) {
   /*** enemy abilities ***/
   //attack ability
   this.attack = function(character) {
-    if (character.defendStatus = true) {
+    if (character.defendStatus == true) {
       character.healthPoints -= (this.attackPower / 2); //note: possibly revisit this
     } else {
       character.healthPoints -= this.attackPower;
     }
-  }
+  };
+
   //defend ability
   this.defend = function(enemy) {
     this.defendStatus = true;
   }
 };
 
-  //tests
-  var andy = new char(10, 2, 3, 1);
-  var baddie = new enemy(10, 2);
-
-  //event listeners for buttons.
-
-  var attackButton = document.querySelector('#attack');
-
-  attackButton.addEventListener("click", function() {
-    andy.attack(baddie);
-    console.log("attack performed, baddie health: " + baddie.healthPoints)
-
-  });
-
-  var defendButton = document.querySelector('#defend');
-
-  defendButton.addEventListener("click", function() {
-    andy.defend();
-    console.log("defend performed.")
-  });
-
-  var healButton = document.querySelector('#heal');
-
-  healButton.addEventListener("click", function() {
-    andy.heal(andy);
-    console.log("heal performed, andy health: " + andy.health)
-
-  });
+//test enemy/characters
+var andy = new char(10, 2, 3, 1);
+var baddie = new enemy(10, 2);
 
 
 
 
-
-
-
-
-
-  //stuff
+//stuff
